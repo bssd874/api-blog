852fcf3c17ad950c95b6a051d20f1991d5f54aa7
bonisteven012 <bonisteven012@gmail.com>
2025-03-26 12:28:25 +0000

    ketiga

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_UPLOAD_SIZE = exports.ALLOWED_IMAGE_TYPES = void 0;
-exports.ALLOWED_IMAGE_TYPES = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
-exports.MAX_UPLOAD_SIZE = 2 * 1024 * 1024; // 2MB
-//# sourceMappingURL=constants.js.map
--- dist/utils/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-var logger = function (message) {
-    console.log("[".concat(new Date().toISOString(), "] ").concat(message));
-};
-exports.logger = logger;
-//# sourceMappingURL=logger.js.map
